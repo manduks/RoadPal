4ee15f3f79e3282ecd7738474a7602d0c82e7866
Armando Gonzalez <iam@armando.mx>
2015-12-09 23:32:47 -0600

    Added configuration for showing messages

--- index.ios.js
+++ index.ios.js
@@ -14,14 +14,93 @@ var {
 } = React;
 
 var RoadPal = React.createClass({
+  getInitialState() {
+    const self = this;
+    var message = '';
+
+    BackgroundGeolocation.configure({
+      desiredAccuracy: 0,
+      stationaryRadius: 50,
+      distanceFilter: 50,
+      disableElasticity: false, // <-- [iOS] Default is 'false'.  Set true to disable speed-based distanceFilter elasticity
+      locationUpdateInterval: 5000,
+      minimumActivityRecognitionConfidence: 80,   // 0-100%.  Minimum activity-confidence for a state-change
+      fastestLocationUpdateInterval: 5000,
+      activityRecognitionInterval: 10000,
+      stopDetectionDelay: 1,  // <--  minutes to delay after motion stops before engaging stop-detection system
+      stopTimeout: 2, // 2 minutes
+      activityType: 'AutomotiveNavigation',
+
+      // Application config
+      debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+      forceReloadOnLocationChange: false,  // <-- [Android] If the user closes the app **while location-tracking is started** , reboot app when a new location is recorded (WARNING: possibly distruptive to user)
+      forceReloadOnMotionChange: false,    // <-- [Android] If the user closes the app **while location-tracking is started** , reboot app when device changes stationary-state (stationary->moving or vice-versa) --WARNING: possibly distruptive to user)
+      forceReloadOnGeofence: false,        // <-- [Android] If the user closes the app **while location-tracking is started** , reboot app when a geofence crossing occurs --WARNING: possibly distruptive to user)
+      stopOnTerminate: false,              // <-- [Android] Allow the background-service to run headless when user closes the app.
+      startOnBoot: true,                   // <-- [Android] Auto start background-service in headless mode when device is powered-up.
+
+      // HTTP / SQLite config
+      url: 'http://posttestserver.com/post.php?dir=cordova-background-geolocation',
+      batchSync: false,       // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
+      autoSync: true,         // <-- [Default: true] Set true to sync each location to server as it arrives.
+      maxDaysToPersist: 1,    // <-- Maximum days to persist a location in plugin's SQLite database when HTTP fails
+      headers: {
+        "X-FOO": "bar"
+      },
+      params: {
+        "auth_token": "maybe_your_server_authenticates_via_token_YES?"
+      }
+    });
+
+    // This handler fires whenever bgGeo receives a location update.
+    BackgroundGeolocation.on('location', function(location) {
+      message = '- [js]location: '+ JSON.stringify(location);
+      console.log(message);
+      self.setState({message: message});
+    });
+
+    // This handler fires whenever bgGeo receives an error
+    BackgroundGeolocation.on('error', function(error) {
+      var type = error.type;
+      var code = error.code;
+      alert(type + " Error: " + code);
+    });
+
+    // This handler fires when movement states changes (stationary->moving; moving->stationary)
+    BackgroundGeolocation.on('motionchange', function(location) {
+        message = '- [js]motionchanged: ' + JSON.stringify(location);
+        console.log(message);
+        self.setState({message: message});
+    });
+
+    BackgroundGeolocation.start(function() {
+      message = '- [js] BackgroundGeolocation started successfully';
+      console.log(message);
+      self.setState({message: message});
+
+      // Fetch current position
+      BackgroundGeolocation.getCurrentPosition({timeout: 30}, function(location) {
+        message = '- [js] BackgroundGeolocation received current position: ' + JSON.stringify(location);
+        console.log(message);
+        self.setState({message: message});
+      }, function(error) {
+        alert("Location error: " + error);
+      });
+    });
+    return {
+      message : 'App started'
+    }
+    // Call #stop to halt all tracking
+    // BackgroundGeolocation.stop();
+  },
   render: function() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Nativee!
+          Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          {this.state.message}
         </Text>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
